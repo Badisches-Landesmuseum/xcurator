87d670fd98e9a119f10e56d246935f5e2b7cd7f5
sraeuchle <sraeuchle@3pc.de>
2023-12-06 12:00:14 +0100

    fix(import): handle timeouts

--- data-enrichment/0-artefact-core-import/src/iiif_handler.py
+++ data-enrichment/0-artefact-core-import/src/iiif_handler.py
@@ -3,8 +3,11 @@ import base64
 import http
 import logging
 from pathlib import Path
+from socket import timeout
+from time import sleep
 from typing import Dict, Optional
 from urllib import request
+from urllib.error import URLError
 
 import pandas as pd
 import requests
@@ -34,7 +37,7 @@ class IIIFHandler:
         }
 
     @staticmethod
-    def base64(iiif_url) -> Optional[str]:
+    def base64(iiif_url, retry: int = 0) -> Optional[str]:
         """download and convert images as web-base64 strings"""
 
         try:
@@ -43,60 +46,78 @@ class IIIFHandler:
         except (http.client.IncompleteRead) as e:
             base64_image = base64.b64encode(e.partial).decode("utf-8")
             base_64_string = f"data:image/jpeg;base64,{base64_image}"
+        except URLError as e:
+            if isinstance(e.reason, timeout):
+                if retry < 3:
+                    logging.error(f'Timeout Error: Data of not retrieved because %s\nURL: %s retry attempt: {retry +1}',  e, iiif_url)
+                    sleep(5)
+                    IIIFHandler.base64(iiif_url, retry+1)
+                else:
+                    logging.error(f'Timeout Error: Max retries exceeded. skip')
+                    print(f"[{e}] -> {iiif_url}")
+                    return None
+            else:
+                print(f"[{e}] -> {iiif_url}")
+                return None
         except Exception as e:
             print(f"[{e.code}] -> {iiif_url}")
             return None
 
         return base_64_string
 
-    @staticmethod
-    def get_all_sizes(df: DataFrame, keep_batch_files: bool = True, batch_size: int = 1000) -> DataFrame:
-        tqdm.pandas()
-        IIIFHandler._get_all(df, "size", batch_size, IIIFHandler.size)
-        return IIIFHandler._merge_batches("*-size-images.csv", keep_batch_files, converters={'size': ast.literal_eval})
 
-    @staticmethod
-    def get_all_base64_images(df: DataFrame, keep_batch_files: bool = True, batch_size: int = 100) -> DataFrame:
-        tqdm.pandas()
-        IIIFHandler._get_all(df, "base64", batch_size, IIIFHandler.base64)
-        return IIIFHandler._merge_batches("*-base64-images.csv", keep_batch_files)
+@staticmethod
+def get_all_sizes(df: DataFrame, keep_batch_files: bool = True, batch_size: int = 1000) -> DataFrame:
+    tqdm.pandas()
+    IIIFHandler._get_all(df, "size", batch_size, IIIFHandler.size)
+    return IIIFHandler._merge_batches("*-size-images.csv", keep_batch_files, converters={'size': ast.literal_eval})
 
-    @staticmethod
-    def _merge_batches(glob_pattern: str, keep_batch_files: bool = True, converters=None) -> DataFrame:
-        files = list(IIIFHandler._temp_directory().rglob(glob_pattern))
-        df = pd.DataFrame()
-        for file in files:
-            try:
-                data = pd.read_csv(file, converters=converters)
-            except:
-                logging.warning(f"Can't read file {file}")
-                continue
-            df = pd.concat([df, data], axis=0)
-            if not keep_batch_files:
-                file.unlink()
-        return df
 
-    @staticmethod
-    def _temp_directory(file: str = None) -> Path:
-        if file:
-            temp_dir = data_dir(f"temp/{file}")
-        else:
-            temp_dir = data_dir("temp")
+@staticmethod
+def get_all_base64_images(df: DataFrame, keep_batch_files: bool = True, batch_size: int = 100) -> DataFrame:
+    tqdm.pandas()
+    IIIFHandler._get_all(df, "base64", batch_size, IIIFHandler.base64)
+    return IIIFHandler._merge_batches("*-base64-images.csv", keep_batch_files)
 
-        return temp_dir
 
-    @staticmethod
-    def _get_all(df: DataFrame, name: str, batch_size: 1000, function):
-        temp_dir = IIIFHandler._temp_directory()
-        temp_dir.mkdir(parents=True, exist_ok=True)
+@staticmethod
+def _merge_batches(glob_pattern: str, keep_batch_files: bool = True, converters=None) -> DataFrame:
+    files = list(IIIFHandler._temp_directory().rglob(glob_pattern))
+    df = pd.DataFrame()
+    for file in files:
+        try:
+            data = pd.read_csv(file, converters=converters)
+        except:
+            logging.warning(f"Can't read file {file}")
+            continue
+        df = pd.concat([df, data], axis=0)
+        if not keep_batch_files:
+            file.unlink()
+    return df
 
-        batches = sliced(range(len(df)), batch_size)
 
-        for index in tqdm(batches):
-            batch_file = IIIFHandler._temp_directory(f"{index.start}-{name}-images.csv")
-            if batch_file.exists():
-                continue
+@staticmethod
+def _temp_directory(file: str = None) -> Path:
+    if file:
+        temp_dir = data_dir(f"temp/{file}")
+    else:
+        temp_dir = data_dir("temp")
 
-            df_slice = df.iloc[index]
-            df_slice[name] = df_slice['image'].apply(function)
-            df_slice.to_csv(batch_file, index=False)
+    return temp_dir
+
+
+@staticmethod
+def _get_all(df: DataFrame, name: str, batch_size: 1000, function):
+    temp_dir = IIIFHandler._temp_directory()
+    temp_dir.mkdir(parents=True, exist_ok=True)
+
+    batches = sliced(range(len(df)), batch_size)
+
+    for index in tqdm(batches):
+        batch_file = IIIFHandler._temp_directory(f"{index.start}-{name}-images.csv")
+        if batch_file.exists():
+            continue
+
+        df_slice = df.iloc[index]
+        df_slice[name] = df_slice['image'].apply(function)
+        df_slice.to_csv(batch_file, index=False)
